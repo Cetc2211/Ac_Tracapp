355b5c991b5b51203938cbb544463807e40c42c4
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-07-28 00:33:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/students/[studentId]/page.tsx
+++ src/app/(main)/students/[studentId]/page.tsx
@@ -31,6 +31,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogFooter,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
