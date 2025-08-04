f086f0d91f8991bbf19c60773857ccc8144a3feb
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-08-04 02:57:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/grades/page.tsx
+++ src/app/(main)/grades/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card';
 import { FilePen, Loader2 } from 'lucide-react';
