a97cdf662902ca7f0953488b021ad0bdb92292b1
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-08-12 07:22:16 +0000

    Con la actualización que hiciste para agregar las observaciones en el me

--- src/lib/placeholder-data.ts
+++ src/lib/placeholder-data.ts
@@ -41,10 +41,19 @@ export const students: Student[] = [
   { id: 'S004', name: 'Diego Fernández', email: 'diego.fernandez@example.com', phone: '555-0104', tutorName: 'Sofia Fernández', tutorPhone: '555-0204', photo: 'https://placehold.co/100x100.png' },
   { id: 'S005', name: 'Mariana López', email: 'mariana.lopez@example.com', phone: '555-0105', tutorName: 'Javier López', tutorPhone: '555-0205', photo: 'https://placehold.co/100x100.png' },
   { id: 'S006', name: 'Jorge Pérez', email: 'jorge.perez@example.com', phone: '555-0106', tutorName: 'Isabel Pérez', tutorPhone: '555-0206', photo: 'https://placehold.co/100x100.png' },
+  { id: 'S007', name: 'Rojas Hernández Isaac Esaú', email: 'isaac.rojas@example.com', phone: '555-0107', tutorName: 'Laura Hernández', tutorPhone: '555-0207', photo: 'https://placehold.co/100x100.png' },
+  { id: 'S008', name: 'Gómez Salas Jimena', email: 'jimena.gomez@example.com', phone: '555-0108', tutorName: 'Roberto Gómez', tutorPhone: '555-0208', photo: 'https://placehold.co/100x100.png' },
 ];
+
+const socialSciencesStudents = [
+    { id: 'S007', name: 'Rojas Hernández Isaac Esaú', email: 'isaac.rojas@example.com', phone: '555-0107', tutorName: 'Laura Hernández', tutorPhone: '555-0207', photo: 'https://placehold.co/100x100.png' },
+    { id: 'S008', name: 'Gómez Salas Jimena', email: 'jimena.gomez@example.com', phone: '555-0108', tutorName: 'Roberto Gómez', tutorPhone: '555-0208', photo: 'https://placehold.co/100x100.png' },
+];
+
 
 export const groups: Group[] = [
   { id: 'G01', subject: 'Matemáticas Avanzadas', students: students.slice(0, 4), semester: 'Tercero', groupName: 'TSPP', facilitator: 'Dr. Alberto Rodriguez' },
   { id: 'G02', subject: 'Historia del Arte', students: students.slice(2, 6), semester: 'Primero', groupName: 'TAEA', facilitator: 'Dra. María Hernandez' },
   { id: 'G03', subject: 'Programación I', students: students.slice(1, 5), semester: 'Primero', groupName: 'TSPA', facilitator: 'Ing. Carlos Sanchez' },
+  { id: 'G04', subject: 'Ciencias Sociales', students: socialSciencesStudents, semester: 'Quinto', groupName: 'TSPC', facilitator: 'Lic. Gabriela Mistral' },
 ];
