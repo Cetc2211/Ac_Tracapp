bd327e6becec7e89cd8a6854603624935564c826
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-08-13 23:26:36 +0000

    Sigue sin crear la cuenta

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   projectId: "academic-tracker-qeoxi",
   appId: "1:263108580734:web:48f982912762db1a038f2f",
-  storageBucket: "academic-tracker-qeoxi.firebasestorage.app",
+  storageBucket: "academic-tracker-qeoxi.appspot.com",
   apiKey: "AIzaSyDalLVD-FcRyYCE26uuK29fEMjh3hDXFcc",
   authDomain: "academic-tracker-qeoxi.firebaseapp.com",
   messagingSenderId: "263108580734"
