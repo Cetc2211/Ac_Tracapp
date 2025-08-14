a09c418b66525590c1c3321c5f63baa3a3130abf
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-08-14 18:25:07 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "academic-tracker-qeoxi",
-  appId: "1:263108580734:web:48f982912762db1a038f2f",
-  storageBucket: "academic-tracker-qeoxi.appspot.com",
-  apiKey: "AIzaSyDalLVD-FcRyYCE26uuK29fEMjh3hDXFcc",
-  authDomain: "academic-tracker-qeoxi.firebaseapp.com",
-  messagingSenderId: "263108580734"
+  "projectId": "academic-tracker-qeoxi",
+  "appId": "1:263108580734:web:48f982912762db1a038f2f",
+  "storageBucket": "academic-tracker-qeoxi.firebasestorage.app",
+  "apiKey": "AIzaSyDalLVD-FcRyYCE26uuK29fEMjh3hDXFcc",
+  "authDomain": "academic-tracker-qeoxi.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "263108580734"
 };
 
 // Initialize Firebase
