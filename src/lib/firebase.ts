65afc52a269397098bb9942ba887c2cf4d57b7a8
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-08-15 01:05:54 +0000

    El mensaje "FirebaseError: Firebase: Error (auth/configuration-not-found

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,18 +3,17 @@ import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": "academic-tracker-qeoxi",
-  "appId": "1:263108580734:web:48f982912762db1a038f2f",
-  "storageBucket": "academic-tracker-qeoxi.firebasestorage.app",
-  "apiKey": "AIzaSyDalLVD-FcRyYCE26uuK29fEMjh3hDXFcc",
-  "authDomain": "academic-tracker-qeoxi.firebaseapp.com",
-  "messagingSenderId": "263108580734"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+// Initialize Firebase for SSR
 const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
-
 
 export { app, auth, db };
