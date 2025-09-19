b9ec42d02c9e25542b79e3825a66d24d90ad3be4
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-19 22:20:50 +0000

    El icono sólo cambió de color, sigue manteniendo La misma letra T CON FO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,8 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
             <DataProvider>
