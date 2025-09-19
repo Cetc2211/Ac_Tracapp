e89eebdd7da2f336a0891efecec8dfe73dd77e36
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-19 22:33:52 +0000

    Sólo cambió a color verde. Si no se puede, pues ni modo, así hay que dej

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png?v=2" />
-        <link rel="icon" href="/favicon.ico?v=2" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body>
             <DataProvider>
