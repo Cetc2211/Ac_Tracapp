b4585c5600973ae87540664c8f400706acb3711e
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-19 22:30:20 +0000

    Tampoco funcionó

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,10 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png?v=2" />
+        <link rel="icon" href="/favicon.ico?v=2" />
       </head>
       <body>
             <DataProvider>
