7b7d59ef74811d3f04f02380e6057074aee39f9c
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-20 00:05:14 +0000

    Pues nada funciona, ya se aburrió el caché, ni nada, hay que muera o qué

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,11 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="manifest" href="/static/manifest.json" />
+        <link rel="apple-touch-icon" href="/static/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
       </head>
       <body>
             <DataProvider>
