920a2706b20a4ae4367c4153edffb511b4d7a9cc
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-20 00:16:02 +0000

    No era eso, te adelantaste de nuevo, te iba decir que en la aplicación e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16x16.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body>
