1e9bcd9fcd94b9a586aa065f810ca77c2d7f5478
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-21 02:17:47 +0000

    Sigue ocurriendo justo tomo la captura de pantalla, le doy borrar imagen

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -141,7 +141,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              "group/sidebar-wrapper flex min-h-screen w-full has-[[data-variant=inset]]:bg-sidebar",
+              "group/sidebar-wrapper flex min-h-screen flex-1 has-[[data-variant=inset]]:bg-sidebar",
               className
             )}
             ref={ref}
