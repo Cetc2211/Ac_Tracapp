ae920bb3e0eaab65c19ab53a30aa268ee9232b50
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-09-28 04:07:53 +0000

    ReferenceError: Can't find variable: activeGroupId

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -50,6 +50,7 @@ const RecordsPage = () => {
     fetchPartialData,
     calculateDetailedFinalGrade,
     activeGroup,
+    activeGroupId,
     activePartialId,
     setActiveGroupId,
     setActivePartialId,
